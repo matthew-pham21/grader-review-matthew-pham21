646d256f458df9d141958b6e0ceaa0a2662265af
matthew-pham21 <matthewpham721@gmail.com>
2023-11-28 16:18:22 -0800

    changed grade.sh

--- grade.sh
+++ grade.sh
@@ -34,11 +34,14 @@ fi
 
 
 java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > ./gradeResults.txt
-resultLine=`grep -n 'Tests run' ./gradeResults.txt`
-runTests1=$(echo ${resultLine} | cut -d ' ' -f 3)
-runTests2=$(echo ${runTests1} | cut -d ',' -f 1)
-failedTests=$(echo ${resultLine} | cut -d ' ' -f 5)
-echo "$(($runTests2 - $failedTests)) / $runTests2 was your test scores!"
+# resultLine=`grep -n 'Tests run' ./gradeResults.txt`
+# runTests1=$(echo ${resultLine} | cut -d ' ' -f 3)
+# runTests2=$(echo ${runTests1} | cut -d ',' -f 1)
+# failedTests=$(echo ${resultLine} | cut -d ' ' -f 5)
+# echo "$(($runTests2 - $failedTests)) / $runTests2 was your test scores!"
+newLine=`grep -n 'Tests' ./gradeResults.txt`
+echoedLine=$(echo ${newLine} | cut -d ':' -f 1)
+echo "$newLine"
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
